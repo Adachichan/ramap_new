42a3c870a95a3f72a1a7b5070267b27c4b7254fa
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-11-07 12:20:20 +0000

    [fix] #11 index_page(public_mymenu) メニュー一覧画面における画像表示（ユーザー）について、アスペクト比を維持した状態で表示するようにコード変更 index_view(public_mymenu) → 引数（画像サイズ）の削除 css(public_mymenu_index_page) → width,heightの指定およびobject-fitの導入

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -8,18 +8,14 @@ class Menu < ApplicationRecord
 
   # メニュー画像に関する実装
   # 画像の中身が空であるか判別
-  def get_menu_image(size)
+  def get_menu_image
     unless menu_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       menu_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
 
-    # 画像のサイズ調整
-    if !size.empty?
-      menu_image.variant(resize: size).processed
-    else
-      menu_image
-    end
+    menu_image
+
   end
 
   def with_tax_price
