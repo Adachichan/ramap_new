cc597fa88826fb9ae05701d12cb0b78bbffdacd9
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-11-07 13:45:55 +0000

    [fix] #14 index_page_and_show_page(public_review) 口コミ一覧および詳細画面における口コミ画像表示（ユーザー）について、アスペクト比を維持した状態で表示するようにコード変更 index_and_show_view(public_review) → 引数（画像サイズ）の削除 css(public_review_index_and_show_page) → width,heightの指定およびobject-fitの導入

--- app/models/review.rb
+++ app/models/review.rb
@@ -13,18 +13,14 @@ class Review < ApplicationRecord
 
   # 口コミ画像に関する実装
   # 画像の中身が空であるか判別
-  def get_review_image(size)
+  def get_review_image
     unless review_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       review_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
 
-    # 画像のサイズ調整
-    if !size.empty?
-      review_image.variant(resize: size).processed
-    else
-      review_image
-    end
+    review_image
+
   end
 
   # 会員IDの存在判定
