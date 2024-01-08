b863ead8c755b607de17e3076fa45f2af637a67f
Adachichan <129758854+Adachichan@users.noreply.github.com>
2024-01-08 14:20:10 +0000

    [deploy_fix] #72_stores_controller(public_show) 主なメニューの表示順を古いものから順に変更

--- app/controllers/public/stores_controller.rb
+++ app/controllers/public/stores_controller.rb
@@ -2,7 +2,7 @@ class Public::StoresController < ApplicationController
 
   def show
     @store = Store.find(params[:id])
-    @menus = @store.menus.order(created_at: :desc).first(6)
+    @menus = @store.menus.order(created_at: :asc).first(6)
   end
 
 end
