6e3a1ec88672a88cfddfad62eb2659d64e68c652
Adachichan <129758854+Adachichan@users.noreply.github.com>
2024-01-09 14:25:42 +0000

    [deploy_fix] #73_development.rb ドメインの引用元変更（Cloud9 → Domain）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,8 +76,9 @@ Rails.application.configure do
   config.hosts.clear
 
   # Don't care if the mailer can't send.
-  host = ENV["Cloud9_HOST"]
-  config.action_mailer.default_url_options = { protocol: 'https', host: host }
+  # host = ENV["Cloud9_HOST"]
+  host = ENV["Domain_HOST"]
+  config.action_mailer.default_url_options = { protocol: 'http', host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
